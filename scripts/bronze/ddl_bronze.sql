e63f0093095b320cb66efeae388187a5ca6babd2
Michael Palocko <palockml@mail.uc.edu>
2025-03-03 18:15:38 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -13,13 +13,13 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.crm_cust_info (
-cst_id				      INT,
-cst_key				      NVARCHAR(50),
-cst_firstname		    NVARCHAR(50),
-cst_lastname		    NVARCHAR(50), 
+cst_id		    INT,
+cst_key		    NVARCHAR(50),
+cst_firstname	    NVARCHAR(50),
+cst_lastname	    NVARCHAR(50), 
 cst_material_status NVARCHAR(50),
-cst_gndr			      NVARCHAR(50),
-cst_create_date		  DATE
+cst_gndr	    NVARCHAR(50),
+cst_create_date	    DATE
 );
 GO
 
@@ -28,13 +28,13 @@ IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.crm_prd_info (
-prd_id		   INT,
-prd_key		   NVARCHAR(50),
-prd_nm		   NVARCHAR(50),
-prd_cost	   INT,
-prd_line	   NVARCHAR(50),
+prd_id	     INT,
+prd_key	     NVARCHAR(50),
+prd_nm	     NVARCHAR(50),
+prd_cost     INT,
+prd_line     NVARCHAR(50),
 prd_start_dt DATETIME,
-prd_end_dt	 DATETIME
+prd_end_dt   DATETIME
 );
 GO
 
@@ -44,14 +44,14 @@ GO
 
 CREATE TABLE bronze.crm_sales_details (
 sls_order_num NVARCHAR(50),
-sls_prd_key	  NVARCHAR(50),
-sls_cust_id	  INT,
+sls_prd_key   NVARCHAR(50),
+sls_cust_id   INT,
 sls_order_dt  INT,
 sls_ship_dt   INT,
 sls_due_dt    INT,
-sls_sales	    INT,
+sls_sales     INT,
 sls_quantity  INT,
-sls_price	    INT
+sls_price     INT
 );
 GO
 
@@ -81,9 +81,9 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-id			    NVARCHAR(50),
-cat			    NVARCHAR(50),
-subcat		  NVARCHAR(50),
+id	    NVARCHAR(50),
+cat	    NVARCHAR(50),
+subcat	    NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
 GO
