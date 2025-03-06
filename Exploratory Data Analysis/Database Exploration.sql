e6941431400c7d116e4e9c6d8a910430372d9973
Michael Palocko <palockml@mail.uc.edu>
2025-03-05 20:12:01 -0500

    Create Database Exploration.sql

---
+++ Exploratory Data Analysis/Database Exploration.sql
@@ -0,0 +1,10 @@
+/* Database Exploration – This helps give us an overview of the database and see how many 
+tables there are, what the names of columns/tables are. This helps provide the foundation 
+of exploring the data.
+*/
+-- Explore all objects in the database
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- Explore all columns in the database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers'
