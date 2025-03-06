2f4f7aad02b4b92e4662232be375f6b904da5189
Michael Palocko <palockml@mail.uc.edu>
2025-03-05 20:24:28 -0500

    Update and rename placeholder to 01_change_over_time_analysis.sql

---
+++ Advanced Data Analysis/01_change_over_time_analysis.sql
@@ -0,0 +1,39 @@
+-- 1) Change-Over-Time Trends
+SELECT 
+	YEAR(order_date) AS order_year,             -- Data type will be INT
+	MONTH(order_date) AS order_month,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+SELECT 
+	DATETRUNC(month, order_date) AS order_date, -- Can use DATETRUNC instead so the month and year are all in one column. Data type will be DATE
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date)
+
+SELECT 
+	FORMAT(order_date, 'yyyy-MMM') AS order_date, -- Can use FORMAT instead so you get whatever specific format you'd like. Data type will be a STRING, so be careful
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
+
+-- How many new customers were added each year?
+SELECT 
+	DATETRUNC(year, order_date) AS create_year,
+	COUNT(customer_key) AS total_customer
+FROM gold.fact_sales
+GROUP BY DATETRUNC(year, order_date)
+ORDER BY DATETRUNC(year, order_date)
